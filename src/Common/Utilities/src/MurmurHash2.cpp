00ff68b058074798a65fa905118978898910739f
Dan Luu <danluu@gmail.com>
2016-05-31 22:42:17 -0700

    Tabs -> spaces on old file.

--- src/Common/Utilities/src/MurmurHash2.cpp
+++ src/Common/Utilities/src/MurmurHash2.cpp
@@ -84,41 +84,41 @@ namespace BitFunnel
 
     uint32_t MurmurHash32A(const void * key, size_t len, unsigned seed)
     {
-	    const uint32_t m = 0x5bd1e995;
-	    const int r = 24;
-	    uint32_t l = static_cast<uint32_t>(len);
+        const uint32_t m = 0x5bd1e995;
+        const int r = 24;
+        uint32_t l = static_cast<uint32_t>(len);
 
-	    const unsigned char * data = (const unsigned char *)key;
+        const unsigned char * data = (const unsigned char *)key;
 
-	    uint32_t h = seed;
+        uint32_t h = seed;
 
-	    while(len >= 4)
-	    {
-		    uint32_t k = *(unsigned int*)data;
+        while(len >= 4)
+        {
+            uint32_t k = *(unsigned int*)data;
 
-		    mmix(h,k);
+            mmix(h,k);
 
-		    data += 4;
-		    len -= 4;
-	    }
+            data += 4;
+            len -= 4;
+        }
 
-	    uint32_t t = 0;
+        uint32_t t = 0;
 
-	    switch(len)
-	    {
-	    case 3: t ^= data[2] << 16;
-	    case 2: t ^= data[1] << 8;
-	    case 1: t ^= data[0];
-	    };
+        switch(len)
+        {
+        case 3: t ^= data[2] << 16;
+        case 2: t ^= data[1] << 8;
+        case 1: t ^= data[0];
+        };
 
-	    mmix(h,t);
-	    mmix(h,l);
+        mmix(h,t);
+        mmix(h,l);
 
-	    h ^= h >> 13;
-	    h *= m;
-	    h ^= h >> 15;
+        h ^= h >> 13;
+        h *= m;
+        h ^= h >> 15;
 
-	    return h;
+        return h;
     }
 
     #undef mmix
