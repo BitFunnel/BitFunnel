e6f37e216e9db3ce4b801e877035517613ac1884
Dan Luu <danluu@gmail.com>
2016-06-13 11:57:01 -0700

    Fix compression TODOs.

--- src/Scripts/simulate.cpp
+++ src/Scripts/simulate.cpp
@@ -2,7 +2,7 @@
 #include <random>
 
 #define MAX_NUM_ROWS 10
-#define NUM_ITERS 1000
+#define NUM_ITERS 10
 #define BLOCK_SIZE 512 // cache line size in bits.
 #define NUM_BLOCKS 10000
 #define NUM_DOCS BLOCK_SIZE * NUM_BLOCKS
@@ -10,28 +10,27 @@
 // DESIGN NOTE: tried using go, but the publicly available binomial rng is approximately 10x slower.
 // TODO: change conventions to match BitFunnel coding conventions.
 
-int run_once(std::mt19937& gen, std::binomial_distribution<int>& base_dist, int num_rows) {
-    int num_accesses;
+int run_once(std::mt19937& gen, std::binomial_distribution<int16_t>& base_dist, int num_rows) {
+    int num_accesses = 0;
 
-    // TODO: reduce int to int16_t?
     // TODO: check to see if doing these allocations inside run_once matters for performance.
     // We could hoist this out and just clear the vectors in here.
-    std::vector<std::vector<int>> universe(num_rows, std::vector<int>(NUM_BLOCKS));
-    for (int j = 0; j < universe[0].size(); ++j) {
-        universe[0][j] = base_dist(gen);
+    std::vector<int16_t> universe(NUM_BLOCKS, 0);
+    for (int i = 0; i < universe.size(); ++i) {
+        universe[i] = base_dist(gen);
         ++num_accesses;
     }
 
-    for (int i = 1; i < universe.size(); ++i) {
-        for (int j = 0; j < universe[0].size(); ++j) {
-            if (universe[i-1][j] == 0) {
-                universe[i][j] = 0;
+    for (int i = 1; i < num_rows; ++i) {
+        for (int j = 0; j < universe.size(); ++j) {
+            if (universe[j] == 0) {
+                // universe[i] = 0;
             } else {
                 ++num_accesses;
-                int row_num_set = base_dist(gen);
-                float previous_fraction = static_cast<float>(universe[i-1][j]) / 512.0;
-                std::binomial_distribution<int> intersection_dist(row_num_set,previous_fraction);
-                universe[i][j] = intersection_dist(gen);
+                int16_t row_num_set = base_dist(gen);
+                float previous_fraction = static_cast<float>(universe[j]) / 512.0;
+                std::binomial_distribution<int16_t> intersection_dist(row_num_set,previous_fraction);
+                universe[j] = intersection_dist(gen);
             }
         }
     }
@@ -42,7 +41,7 @@ int main()
 {
     std::random_device rd;
     std::mt19937 gen(rd());
-    std::binomial_distribution<int> base_dist(511, 0.2);
+    std::binomial_distribution<int16_t> base_dist(511, 0.2);
 
     for (int i = 1; i <= MAX_NUM_ROWS; ++i) {
         std::cout << i;
