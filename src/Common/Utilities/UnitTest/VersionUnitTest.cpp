ef2dfb3942bc595d2d685171b21461970f12f6e0
Dan Luu <danluu@gmail.com>
2016-06-24 21:53:18 -0700

    Port VersionUnitTest.

--- src/Common/Utilities/UnitTest/VersionUnitTest.cpp
+++ src/Common/Utilities/UnitTest/VersionUnitTest.cpp
@@ -1,16 +1,15 @@
-#include "stdafx.h"
-
 #include <sstream>
 
-#include "BitFunnel/Version.h"
-#include "SuiteCpp/UnitTest.h"
+#include "gtest/gtest.h"
+
+#include "BitFunnel/Utilities/Version.h"
 
 
 namespace BitFunnel
 {
     namespace VersionUnitTest
     {
-        TestCase(Comprehensive)
+        TEST(Manual, Trivial)
         {
             Version version(1, 1, 0);
 
@@ -19,33 +18,33 @@ namespace BitFunnel
 
             Version version2(output);
 
-            TestAssert(version.VersionMajor() == version2.VersionMajor());
-            TestAssert(version.VersionMiddle() == version2.VersionMiddle());
-            TestAssert(version.VersionMinor() == version2.VersionMinor());
+            EXPECT_EQ(version.VersionMajor(), version2.VersionMajor());
+            EXPECT_EQ(version.VersionMiddle(), version2.VersionMiddle());
+            EXPECT_EQ(version.VersionMinor(), version2.VersionMinor());
 
             Version version3(1, 1, 1);
-            TestAssert(version.IsCompatibleWith(version3));
+            EXPECT_TRUE(version.IsCompatibleWith(version3));
 
             Version version4(1, 2, 0);
-            TestAssert(!version.IsCompatibleWith(version4));
+            EXPECT_TRUE(!version.IsCompatibleWith(version4));
 
             Version version5(2, 1, 0);
-            TestAssert(!version.IsCompatibleWith(version5));
+            EXPECT_TRUE(!version.IsCompatibleWith(version5));
 
             Version version6 = version.IncrementMajor();
-            TestAssert(version6.VersionMajor() == 2);
-            TestAssert(version6.VersionMiddle() == 0);
-            TestAssert(version6.VersionMinor() == 0);
+            EXPECT_EQ(version6.VersionMajor(), 2);
+            EXPECT_EQ(version6.VersionMiddle(), 0);
+            EXPECT_EQ(version6.VersionMinor(), 0);
 
             Version version7 = version.IncrementMiddle();
-            TestAssert(version7.VersionMajor() == 1);
-            TestAssert(version7.VersionMiddle() == 2);
-            TestAssert(version7.VersionMinor() == 0);
+            EXPECT_EQ(version7.VersionMajor(), 1);
+            EXPECT_EQ(version7.VersionMiddle(), 2);
+            EXPECT_EQ(version7.VersionMinor(), 0);
 
             Version version8 = version.IncrementMinor();
-            TestAssert(version8.VersionMajor() == 1);
-            TestAssert(version8.VersionMiddle() == 1);
-            TestAssert(version8.VersionMinor() == 1);
+            EXPECT_EQ(version8.VersionMajor(), 1);
+            EXPECT_EQ(version8.VersionMiddle(), 1);
+            EXPECT_EQ(version8.VersionMinor(), 1);
         }
     }
 }
