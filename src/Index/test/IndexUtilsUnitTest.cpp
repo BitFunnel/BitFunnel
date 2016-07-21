1553aa736bb4a6f2c127c4f3e8680d556559fea1
Dan Luu <danluu@gmail.com>
2016-07-20 19:41:26 -0700

    Add IndexUtilsUnitTest.

---
+++ src/Index/test/IndexUtilsUnitTest.cpp
@@ -0,0 +1,54 @@
+#include "gtest/gtest.h"
+
+#include "BitFunnel/BitFunnelTypes.h"  // For DocIndex.
+#include "BitFunnel/RowId.h"  // For RowIndex.
+#include "DocTableDescriptor.h"
+#include "DocumentDataSchema.h"
+#include "IndexUtils.h"
+#include "Mocks/EmptyTermTable.h"
+#include "Shard.h"
+
+namespace BitFunnel
+{
+    namespace IndexUtilsUnitTest
+    {
+        TEST(BufferSizeTest, Trivial)
+        {
+            static const DocIndex c_capacityQuanta = 4096;
+            static const size_t c_sizeOfSlicePtr = sizeof(void*);
+            DocumentDataSchema schema;
+            schema.RegisterVariableSizeBlob();
+            schema.RegisterFixedSizeBlob(10);
+
+            {
+                // When there are no rows, buffer size is driven only by DocTable.
+                const size_t expectedBufferSize = DocTableDescriptor::GetBufferSize(c_capacityQuanta * 1, schema) + c_sizeOfSlicePtr;
+                EXPECT_EQ(GetEmptyTermTableBufferSize(c_capacityQuanta * 1, { 0, 0, 0, 0, 0, 0, 0 }, schema), expectedBufferSize);
+            }
+
+            {
+                // 1 row at rank 0.
+                const size_t expectedBufferSize = DocTableDescriptor::GetBufferSize(c_capacityQuanta * 1, schema) + 4096 / 8 + c_sizeOfSlicePtr;
+                EXPECT_EQ(GetEmptyTermTableBufferSize(c_capacityQuanta * 1, { 1, 0, 0, 0, 0, 0, 0 }, schema), expectedBufferSize);
+            }
+
+            {
+                // 10 row at rank 0.
+                const size_t expectedBufferSize = DocTableDescriptor::GetBufferSize(c_capacityQuanta * 1, schema) + 4096 / 8 * 10 + c_sizeOfSlicePtr;
+                EXPECT_EQ(GetEmptyTermTableBufferSize(c_capacityQuanta * 1, { 10, 0, 0, 0, 0, 0, 0 }, schema), expectedBufferSize);
+            }
+
+            {
+                // 1 row at rank 0, 10 rows at rank 3.
+                const size_t expectedBufferSize = DocTableDescriptor::GetBufferSize(c_capacityQuanta * 1, schema) + 4096 / 8 + 4096 / 8 / 8 * 10 + c_sizeOfSlicePtr;
+                EXPECT_EQ(GetEmptyTermTableBufferSize(c_capacityQuanta * 1, { 1, 0, 0, 10, 0, 0, 0 }, schema), expectedBufferSize);
+            }
+
+            {
+                // 20 rows at rank 6.
+                const size_t expectedBufferSize = DocTableDescriptor::GetBufferSize(c_capacityQuanta * 2, schema) + 2 * 4096 / 8 / 64 * 20 + c_sizeOfSlicePtr;
+                EXPECT_EQ(GetEmptyTermTableBufferSize(c_capacityQuanta * 2, { 0, 0, 0, 0, 0, 0, 20 }, schema), expectedBufferSize);
+            }
+        }
+    }
+}
