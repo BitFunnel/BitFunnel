90bc343f4b6280181447b5a4e9e9b7f1150be10a
Dan Luu <danluu@gmail.com>
2016-08-12 15:13:58 -0700

    Meaningless edits to plotting script.

--- src/Scripts/plot-sim.r
+++ src/Scripts/plot-sim.r
@@ -2,8 +2,10 @@ library("ggplot2")
 library("reshape2")
 setwd("~/dev/BitFunnel/src/Scripts")
 
-png(filename="wat.png",width=800,height=600)
-df <- read.csv(header=FALSE, file="wat.csv")
+# png(filename="wat.png",width=800,height=600)
+png(filename="talk.png",width=1600,height=1200)
+# df <- read.csv(header=FALSE, file="wat.csv")
+df <- read.csv(header=FALSE, file="talk.csv")
 ## df <- read.csv(file="wat.csv")
 ## munged <- melt(df)
 ## ggplot(munged, aes(x=variable, y=value)) + geom_point()
@@ -12,6 +14,10 @@ df <- read.csv(header=FALSE, file="wat.csv")
 # ggplot(data=df, aes(x=factor(V1), y=V2)) + geom_bar(stat="identity") + labs(x = "Number of cache misses", y = "count")
 
 # plot uniform 20 vs. buggy distribution
-ggplot(data=df, aes(x=factor(V1), y=V2)) + geom_bar(stat="identity") + facet_wrap(~ V3, ncol=1) + labs(x = "Number of cache misses", y = "count")
+# ggplot(data=df, aes(x=factor(V1), y=V2)) + geom_bar(stat="identity") + facet_wrap(~ V3, ncol=1) + labs(x = "Number of cache misses", y = "count")
+
+# plot for talk
+ggplot(data=df, aes(x=factor(V1), y=V2)) + geom_bar(stat="identity") + labs(x = "Memory accesses", y = "Count") +
+  theme(axis.text.x = element_text(size=40))
 
 dev.off()
