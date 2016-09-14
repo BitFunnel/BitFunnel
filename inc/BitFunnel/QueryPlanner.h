f3bd3b4bd8cb84b6413ca69db0fb4e36c072910a
Dan Luu <danluu@gmail.com>
2016-09-14 12:19:08 -0700

    Initial copy of QueryPlanner.

---
+++ inc/BitFunnel/QueryPlanner.h
@@ -0,0 +1,88 @@
+#pragma once
+
+#include "BitFunnel/NonCopyable.h"        // Inherits from NonCopyable.
+
+
+namespace X64
+{
+    class X64FunctionGenerator;
+}
+
+
+namespace BitFunnel
+{
+    namespace Allocators
+    {
+        class IAllocator;
+    }
+
+    class IIndexConfiguration;
+    class IPlanRows;
+    class IThreadResources;
+    class TermPlan;
+    class X64::X64FunctionGenerator;
+
+    class QueryPlanner : public NonCopyable
+    {
+    public:
+        // Constructs a QueryPlanner with the specified resources.
+        QueryPlanner(TermPlan const & termPlan,
+                     unsigned targetRowCount,
+                     IIndexConfiguration const & index,
+                     IThreadResources& threadResources,
+                     Allocators::IAllocator& allocator,
+                     IDiagnosticStream* diagnosticStream,
+                     bool generateNonBodyPlan,
+                     unsigned maxIterationsScannedBetweenTerminationChecks);
+
+        //
+        // IQueryPlanner methods.
+        //
+
+        const CompiledFunction GetMatchingFunction() const;
+
+        IPlanRows const & GetPlanRows() const;
+
+    private:
+
+        // Wrapper class for X64FunctionGenerator to manage the 
+        // allocate and release of the X64FunctionGenerator object in
+        // ThreadResources.
+        // The goal of this class is to be an RAII wrapper to protect 
+        // against throws in the constructor of QueryPlanner.
+        class X64FunctionGeneratorWrapper : public NonCopyable
+        {
+        public:
+            // The constructor takes a X64FunctionGenerator from thread resources.
+            X64FunctionGeneratorWrapper(IThreadResources& threadResources);
+
+            // The destructor return the X64FunctionGenerator back to the thread resources.
+            ~X64FunctionGeneratorWrapper();
+
+            operator X64::X64FunctionGenerator&() const;
+
+        private:
+            // The X64FunctionGenerator maintains the executable buffers of X64
+            X64::X64FunctionGenerator& m_code;
+
+            // Stored so that the X64FunctionGenerator can be released during destruction
+            IThreadResources& m_threadResources;
+        };
+
+        // The X64FunctionGeneratorWrapper which wraps a x64FunctionGenerator allocated from
+        // thread resources. It also is responsible for release the wrapped x64FunctionGenerator.
+        X64FunctionGeneratorWrapper m_x64FunctionGeneratorWrapper;
+
+        IPlanRows const * m_planRows;
+
+        // The maximum number of iterations that can be performed before a termination
+        // check is mandatory. Details can be found in the MatchTreeCodeGenerator.
+        const unsigned m_maxIterationsScannedBetweenTerminationChecks;
+
+        // First available row pointer register is R8.
+        static const unsigned c_registerBase = 8;
+
+        // Row pointers stored in the eight registers R8..R15.
+        static const unsigned c_registerCount = 8;
+    };
+}
---
+++ src/Plan/src/QueryPlanner.cpp
@@ -0,0 +1,189 @@
+#include "stdafx.h"
+
+
+#include "BitFunnelAllocatorInterfaces/IAllocator.h"
+#include "BitFunnel/BitFunnelErrors.h"
+#include "BitFunnel/CompiledFunction.h"
+#include "BitFunnel/Factories.h"
+#include "BitFunnel/IDiagnosticStream.h"
+#include "BitFunnel/IObjectFormatter.h"
+#include "BitFunnel/IPlanRows.h"
+#include "BitFunnel/IThreadResources.h"
+#include "BitFunnel/QueryPlanner.h"
+#include "BitFunnel/RowPlan.h"
+#include "BitFunnel/TermMatchNodes.h"
+#include "BitFunnel/TermPlan.h"
+#include "BitFunnel/TermPlanConverter.h"
+#include "BitFunnel/Tier.h"
+#include "CompileNodes.h"
+#include "LoggerInterfaces/Logging.h"
+#include "MachineCodeGenerator.h"
+#include "MatchTreeCodeGenerator.h"
+#include "MatchTreeRewriter.h"
+#include "RankDownCompiler.h" 
+#include "RegisterAllocator.h"
+#include "RowPlanCompiler.h"
+
+
+namespace BitFunnel
+{
+    QueryPlanner::X64FunctionGeneratorWrapper::X64FunctionGeneratorWrapper(IThreadResources& threadResources)
+        : m_code(threadResources.AllocateFunctionGenerator()),
+          m_threadResources(threadResources) 
+    {
+    }
+
+
+    QueryPlanner::X64FunctionGeneratorWrapper::~X64FunctionGeneratorWrapper()
+    {
+        m_threadResources.ReleaseFunctionGenerator(m_code);
+    }
+
+
+    QueryPlanner::X64FunctionGeneratorWrapper::operator X64::X64FunctionGenerator&() const
+    {
+        return m_code;
+    }
+
+
+    unsigned const c_targetCrossProductTermCount = 180;
+
+    QueryPlanner::QueryPlanner(TermPlan const & termPlan,
+                               unsigned targetRowCount,
+                               IIndexConfiguration const & index,
+                               IThreadResources& threadResources,
+                               Allocators::IAllocator& allocator,
+                               IDiagnosticStream* diagnosticStream,
+                               bool generateNonBodyPlan,
+                               unsigned maxIterationsScannedBetweenTerminationChecks)
+        : m_x64FunctionGeneratorWrapper(threadResources),
+          m_maxIterationsScannedBetweenTerminationChecks(maxIterationsScannedBetweenTerminationChecks)
+    {
+        std::auto_ptr<IObjectFormatter> formatter;
+
+        if (diagnosticStream != nullptr && diagnosticStream->IsEnabled("planning/term"))
+        {
+            std::ostream& out = diagnosticStream->GetStream();
+            std::auto_ptr<IObjectFormatter>
+                formatter(Factories::CreateObjectFormatter(diagnosticStream->GetStream()));
+
+            out << "--------------------" << std::endl;
+            out << "Term Plan:" << std::endl;
+            termPlan.GetMatchTree().Format(*formatter);
+            out << std::endl;
+        }
+
+        RowPlan const & rowPlan = 
+            TermPlanConverter::BuildRowPlan(termPlan.GetMatchTree(), 
+                                            index, 
+                                            generateNonBodyPlan, 
+                                            allocator);
+
+        if (diagnosticStream != nullptr && diagnosticStream->IsEnabled("planning/row"))
+        {
+            std::ostream& out = diagnosticStream->GetStream();
+            std::auto_ptr<IObjectFormatter>
+                formatter(Factories::CreateObjectFormatter(diagnosticStream->GetStream()));
+
+            out << "--------------------" << std::endl;
+            out << "Row Plan:" << std::endl;
+            rowPlan.Format(*formatter);
+            out << std::endl;
+        }
+
+        m_planRows = &rowPlan.GetPlanRows();
+
+        if (diagnosticStream != nullptr && diagnosticStream->IsEnabled("planning/planrows"))
+        {
+            std::ostream& out = diagnosticStream->GetStream();
+            std::auto_ptr<IObjectFormatter>
+                formatter(Factories::CreateObjectFormatter(diagnosticStream->GetStream()));
+
+            out << "--------------------" << std::endl;
+            out << "IPlanRows:" << std::endl;
+            for (ShardId shard = 0 ; shard < m_planRows->GetShardCount(); ++shard)
+            {
+                for (unsigned id = 0 ; id < m_planRows->GetRowCount(); ++id)
+                {
+                    RowId row = m_planRows->PhysicalRow(shard, id);
+
+                    out << "(" << shard << ", " << id << "): ";
+                    if (row.GetTier() == InvalidTier)
+                    {
+                        out << "(no row)" << std::endl;
+                    }
+                    else
+                    {
+                        out << "RowId(" << row.GetShard();
+                        out << ", " << row.GetRank();
+                        out << ", " << TierToString(row.GetTier());
+                        out << ", " << row.GetIndex() << ")" << std::endl;
+                    }
+                }
+            }
+        }
+
+        // Rewrite match tree to optimal form for the RankDownCompiler.
+        RowMatchNode const & rewritten = 
+            MatchTreeRewriter::Rewrite(rowPlan.GetMatchTree(),
+                                       targetRowCount,
+                                       c_targetCrossProductTermCount,
+                                       allocator);
+
+
+        if (diagnosticStream != nullptr && diagnosticStream->IsEnabled("planning/rewrite"))
+        {
+            std::ostream& out = diagnosticStream->GetStream();
+            std::auto_ptr<IObjectFormatter>
+                formatter(Factories::CreateObjectFormatter(diagnosticStream->GetStream()));
+
+            out << "--------------------" << std::endl;
+            out << "Rewritten Plan:" << std::endl;
+            rewritten.Format(*formatter);
+            out << std::endl;
+        }
+
+        // Compile the match tree into CompileNodes.
+        RankDownCompiler compiler(allocator);
+        compiler.Compile(rewritten);
+        CompileNode const & compileTree = compiler.CreateTree(c_maxRankValue);
+
+        if (diagnosticStream != nullptr && diagnosticStream->IsEnabled("planning/compile"))
+        {
+            std::ostream& out = diagnosticStream->GetStream();
+            std::auto_ptr<IObjectFormatter>
+                formatter(Factories::CreateObjectFormatter(diagnosticStream->GetStream()));
+
+            out << "--------------------" << std::endl;
+            out << "Compile Nodes:" << std::endl;
+            compileTree.Format(*formatter);
+            out << std::endl;
+        }
+
+        // Perform register allocation on the compile tree.
+        RegisterAllocator const registers(compileTree,
+                                          rowPlan.GetPlanRows().GetRowCount(),
+                                          c_registerBase,
+                                          c_registerCount,
+                                          allocator);
+
+        // Finally, translate compile tree to X64 machine code.
+        MatchTreeCodeGenerator generator(registers,
+                                         m_x64FunctionGeneratorWrapper,
+                                         m_maxIterationsScannedBetweenTerminationChecks);
+        generator.GenerateX64Code(compileTree);
+    }
+
+
+    const CompiledFunction QueryPlanner::GetMatchingFunction() const
+    {
+        // TODO: Don't like how call to GetMatchingFunction() finalizes jumps. This should be explicit.
+        return CompiledFunction((X64::X64FunctionGenerator&)m_x64FunctionGeneratorWrapper);
+    }
+
+
+    IPlanRows const & QueryPlanner::GetPlanRows() const
+    {
+        return *m_planRows;
+    }
+}
