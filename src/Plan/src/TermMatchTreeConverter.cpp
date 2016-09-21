95db0dc843ccc04f69944da215c9d308eba40136
Dan Luu <danluu@gmail.com>
2016-09-21 12:43:35 -0700

    Add TODO on method.

--- src/Plan/src/TermMatchTreeConverter.cpp
+++ src/Plan/src/TermMatchTreeConverter.cpp
@@ -39,7 +39,7 @@ namespace BitFunnel
     {
          RowMatchNode::Builder builder(RowMatchNode::AndMatch, m_allocator);
 
-         // It is important to build a node for the soft-deleted row first 
+         // It is important to build a node for the soft-deleted row first
          // because there are certain optimizations which cause some of the rows
          // to be ignored (e.g. due to number of rows exceeding the limit) and
          // the soft-deleted row must always be added.
@@ -186,12 +186,15 @@ namespace BitFunnel
     }
 
 
+    // TODO: is this method needed at all? In the old codebase, there was a
+    // giant switch based on Classification in order to determine the Tier. The
+    // rewrite contains neither Tier nor Classification.
     const Term TermMatchTreeConverter::GetUnigramTerm(char const * text, char const * suffix, Classification classification) const
     {
         Term::Hash termHash = Term::ComputeQualifiedRawHash(text, suffix);
         IdfX10 termIdf = m_index.GetIndexedDocFreqTable().LookupIdf(termHash);
 
-        const Stream::Classification termClassification = 
+        const Stream::Classification termClassification =
             ClassificationToStreamClassification(classification);
 
         Tier termTier;
@@ -255,8 +258,8 @@ namespace BitFunnel
 
     void TermMatchTreeConverter::AppendTermRows(RowMatchNode::Builder& builder, const Term& term)
     {
-        // The m_planRows.IsFull() check is an optimization to avoid unnecessary work for the rows 
-        // which will be ignored in case that the PlanRows is full inside AbstractRowEnumerator. 
+        // The m_planRows.IsFull() check is an optimization to avoid unnecessary work for the rows
+        // which will be ignored in case that the PlanRows is full inside AbstractRowEnumerator.
         // Theoretically, this check can be added at an even higher level in the code such as
         // BuildMatchTree(Unigram) or BuildMatchTree(Phrase). We add this optimization code here because
         // we think checking the PlanRow is full or not at this level is already good enough
@@ -274,20 +277,20 @@ namespace BitFunnel
 
     void TermMatchTreeConverter::AppendTermRows(RowMatchNode::Builder& builder, const FactHandle& fact)
     {
-        // The m_planRows.IsFull() check is an optimization to avoid unnecessary work for the rows 
-        // which will be ignored in case that the PlanRows is full inside AbstractRowEnumerator. 
+        // The m_planRows.IsFull() check is an optimization to avoid unnecessary work for the rows
+        // which will be ignored in case that the PlanRows is full inside AbstractRowEnumerator.
         // Theoretically, this check can be added at an even higher level in the code such as
         // BuildMatchTree(Unigram) or BuildMatchTree(Phrase). We add this optimization code here because
         // we think checking the PlanRow is full or not at this level is already good enough
         // from a performance point of view.
-        // TODO TFS 16063. Consider returning true/false from these methods 
+        // TODO TFS 16063. Consider returning true/false from these methods
         // when IPlanRows is full. Consider combining Term/Fact code paths together.
         if (!m_planRows.IsFull())
         {
             AbstractRowEnumerator rowEnumerator(fact, m_planRows);
             while (rowEnumerator.MoveNext())
             {
-                builder.AddChild(RowMatchNode::Builder::CreateRowNode(rowEnumerator.Current(), 
+                builder.AddChild(RowMatchNode::Builder::CreateRowNode(rowEnumerator.Current(),
                                                                       m_allocator));
             }
         }
