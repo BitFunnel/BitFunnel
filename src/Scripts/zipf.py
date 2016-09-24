c20ea594004d882edbf1224f2cbd709b33d16615
Dan Luu <danluu@gmail.com>
2016-09-23 18:55:28 -0700

    Find average f.

--- src/Scripts/zipf.py
+++ src/Scripts/zipf.py
@@ -1,9 +1,34 @@
 from collections import defaultdict
 import numpy as np
 
-postings = defaultdict(int)
-for _ in range (10000):
-    postings[np.random.zipf(1.2)] += 1
+num_increments = 12
+outer_trials = 10
+inner_trials = outer_trials * 100
+zipf_parameter = 2
+running_sum = defaultdict(int)
+num_entries = defaultdict(int)
 
-for key, value in postings.items():
-    print('{},{}'.format(key, value))
+def add_postings(postings, old_num, new_num):
+    for _ in range (old_num, new_num):
+        postings[np.random.zipf(zipf_parameter)] += 1
+
+for _ in range (outer_trials):
+    postings = defaultdict(int)
+    num_postings_last = 0
+    num_postings = 1024
+    add_postings(postings, num_postings_last, num_postings)
+
+    for _ in range (num_increments):
+        num_postings_last = num_postings
+        num_postings *= 2
+        add_postings(postings, num_postings_last, num_postings)
+
+        for _ in range (inner_trials):
+            draw = np.random.zipf(zipf_parameter)
+            local_num = postings[draw]
+            # print('{},{}'.format(num_postings, postings[draw]))
+            running_sum[num_postings] += local_num
+            num_entries[num_postings] += num_postings
+
+for key in sorted(running_sum):
+    print('{},{}'.format(key, running_sum[key]/num_entries[key]))
