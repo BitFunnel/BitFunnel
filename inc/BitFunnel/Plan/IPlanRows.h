2cd9728b0d9cbf237a5ca117e253fa2f4f2899ea
Dan Luu <danluu@gmail.com>
2016-09-25 17:05:49 -0700

    Initial copy of IPlanRows.

---
+++ inc/BitFunnel/Plan/IPlanRows.h
@@ -0,0 +1,33 @@
+#pragma once
+
+#include <iosfwd>
+
+#include "BitFunnel/AbstractRow.h"
+#include "BitFunnel/BitFunnelTypes.h"
+#include "BitFunnel/RowId.h"
+
+
+namespace BitFunnel
+{
+    class ITermTable;
+
+    class IPlanRows
+    {
+    public:
+        virtual ~IPlanRows() {};
+
+        virtual ShardId GetShardCount() const = 0;
+        virtual unsigned GetRowCount() const = 0;
+
+        virtual const ITermTable& GetTermTable(ShardId shard) const = 0;
+
+        virtual bool IsFull() const = 0;
+        virtual AbstractRow AddRow(Rank rank) = 0;
+
+        virtual RowId const & PhysicalRow(ShardId shard, unsigned id) const = 0;
+        virtual RowId& PhysicalRow(ShardId shard, unsigned id) = 0;
+
+        // Writes the PlanRows data to a stream.
+        virtual void Write(std::ostream& stream) const = 0;
+    };
+}
