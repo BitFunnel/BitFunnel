92ca5a2de1b05c316c6b381e10b96d1955056972
Dan Luu <danluu@gmail.com>
2016-10-17 19:47:02 -0700

    Initial copy of IShardIndex.

---
+++ inc/BitFunnel/Index/IShardIndex.h
@@ -0,0 +1,47 @@
+#pragma once
+
+#include <memory>                         // For ptrdiff_t.
+#include <vector>                         // For function return value.
+
+#include "BitFunnel/BitFunnelTypes.h"     // ShardId and DocIndex parameter.
+#include "BitFunnel/IInterface.h"
+#include "BitFunnel/RowId.h"
+
+
+namespace BitFunnel
+{
+    class IIngestionIndex;
+    class ITokenManager;
+    class Shard;
+
+    //*************************************************************************
+    //
+    // IShardIndex is a base abstract class or interface for classes that 
+    // maintain dynamic portion of the BitFunnel index for a particular shard. 
+    // This data is used in query serving.
+    //
+    //*************************************************************************
+    class IShardIndex : public IInterface
+    {
+    public:
+        // Returns the Id of the shard.
+        virtual ShardId GetId() const = 0;
+
+        // Returns capacity of a single Slice in the Shard. All Slices in the 
+        // Shard have the same capacity.
+        virtual DocIndex GetSliceCapacity() const = 0;
+
+        // Returns a vector of slice buffers for this shard.
+        // The callers needs to obtain a Token from ITokenManager to protect
+        // the pointer to the list of slice buffers, as well as the buffers 
+        // themselves.
+        virtual std::vector<void*> const & GetSliceBuffers() const = 0;
+
+        // Returns the offset of the row in the slice buffer in a shard.
+        virtual ptrdiff_t GetRowOffset(RowId rowId) const = 0;
+
+        // Returns the offset in the slice buffer where a pointer to the Slice
+        // is stored. This is the same offset for all slices in the Shard.
+        virtual ptrdiff_t GetSlicePtrOffset() const = 0;
+    };
+}
