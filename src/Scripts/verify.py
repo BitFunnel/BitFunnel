764809e477f73d68596e3a2bf1609125fe5d2f56
Dan Luu <danluu@gmail.com>
2016-11-15 16:09:53 -0800

    Minor optimizations. 20% speedup of verification.

--- src/Scripts/verify.py
+++ src/Scripts/verify.py
@@ -25,46 +25,45 @@ def get_true_matches(filename):
                 true_matches[row[0]].add(row[1])
     return true_matches
 
+def process_unknowns(filename, true_matches):
+    with open(filename) as f:
+        cf = open("/tmp/sum.csv", 'w', newline='')
+        writer = csv.writer(cf)
+        writer.writerow(["Query","TermPos","TruePositives","FalsePositives","FalseNegatives","FalseRate"])
+        reader = csv.reader(f)
+        last_term = ""
+        num_true_pos = 0
+        num_false_pos = 0
+        num_false_neg = 0 # TODO: handle.
+        term_pos = -1
+        for row in reader:
+            # print(row)
+            # TODO: assert that value is '3'
+            #
+            # TODO: handle false negatives. Could keep a counter of how many matches
+            # we've seen and compare, then iterate over the set in the rare instance
+            # we see a false negative.
+            #
+            # TODO: handle last term.
+            if row[0] != last_term:
+                if (term_pos > 0):
+                    writer.writerow([last_term,
+                                     term_pos,
+                                     num_true_pos,
+                                     num_false_pos,
+                                     num_false_neg,
+                                     num_false_pos / (num_true_pos + num_false_pos)])
+                num_true_pos = 0
+                num_false_pos = 0
+                num_false_neg = 0 # TODO: handle.
+                term_pos += 1
+
+            if (len(row) == 3):
+                if row[1] in true_matches[row[0]]:
+                    num_true_pos += 1
+                else:
+                    num_false_pos += 1
+            last_term = row[0]
+
 true_matches = get_true_matches("/tmp/groundTruth.csv")
-
-with open("/tmp/unknown.csv") as f:
-    cf = open("/tmp/sum.csv", 'w', newline='')
-    writer = csv.writer(cf)
-    writer.writerow(["Query","TermPos","TruePositives","FalsePositives","FalseNegatives","FalseRate"])
-    reader = csv.reader(f)
-    last_term = ""
-    num_true_pos = 0
-    num_false_pos = 0
-    num_false_neg = 0 # TODO: handle.
-    term_pos = -1
-    for row in reader:
-        # print(row)
-        # TODO: assert that value is '3'
-        #
-        # TODO: handle false negatives. Could keep a counter of how many matches
-        # we've seen and compare, then iterate over the set in the rare instance
-        # we see a false negative.
-        #
-        # TODO: handle last term.
-        if row[0] != last_term:
-            if (last_term == "proglet"):
-                print(num_true_pos)
-                print(num_false_pos)
-            if (term_pos > 0):
-                writer.writerow([last_term,
-                                term_pos,
-                                num_true_pos,
-                                num_false_pos,
-                                num_false_neg,
-                                num_false_pos / (num_true_pos + num_false_pos)])
-            num_true_pos = 0
-            num_false_pos = 0
-            num_false_neg = 0 # TODO: handle.
-            term_pos += 1
-
-        if (len(row) == 3):
-            if row[1] in true_matches[row[0]]:
-                num_true_pos += 1
-            else:
-                num_false_pos += 1
-        last_term = row[0]
+process_unknowns("/tmp/unknown.csv", true_matches)
