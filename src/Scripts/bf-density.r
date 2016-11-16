d092878ba565341b70db5af13c47ff12d67a5ef7
Dan Luu <danluu@gmail.com>
2016-11-15 16:22:49 -0800

    Make plot legends legible.

--- src/Scripts/bf-density.r
+++ src/Scripts/bf-density.r
@@ -5,19 +5,25 @@ df <- read.csv(header=TRUE, file="sum.csv")
 png(filename="true-matches.png",width=1600,height=1200)
 ggplot(df, aes(TermPos,TruePositives)) +
     geom_point(alpha=1/5) +
-    theme_bw()
+    theme_bw() +
+    theme(axis.text = element_text(size=40),
+          axis.title = element_text(size=40))
 dev.off()
 
 
 png(filename="false-rate.png",width=1600,height=1200)
 ggplot(df, aes(TermPos,FalseRate)) +
     geom_point(alpha=1/100) +
-    theme_bw()
+    theme_bw() +
+    theme(axis.text = element_text(size=40),
+          axis.title = element_text(size=40))
 dev.off()
 
 
 png(filename="false-matches.png",width=1600,height=1200)
 ggplot(df, aes(TermPos,FalsePositives)) +
     geom_point(alpha=1/5) +
-    theme_bw()
+    theme_bw() +
+    theme(axis.text = element_text(size=40),
+          axis.title = element_text(size=40))
 dev.off()
