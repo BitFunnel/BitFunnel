6c8dd596a0f5f84acee54938d2f948f25445327d
Dan Luu <danluu@gmail.com>
2016-11-21 14:17:15 -0800

    Put multple treatments into the same histogram.

--- src/Scripts/correlation-histogram.py
+++ src/Scripts/correlation-histogram.py
@@ -3,28 +3,42 @@
 from collections import defaultdict
 import csv
 
-term_term_correlation = defaultdict(int)
-term_all_correlation = defaultdict(int)
+term_term_correlation = defaultdict(lambda:defaultdict(int))
+term_all_correlation = defaultdict(lambda:defaultdict(int))
 
-# TODO: don't hardcode name.
-with open("/tmp/Correlate-0.csv") as f:
-    reader = csv.reader(f)
-    for row in reader:
-        term_all = 0
-        pos = 0
-        for item in row:
-            if pos > 0 and pos % 2 == 0:
-                correlation = int(item)
-                term_all += correlation
-                term_term_correlation[correlation] += 1
-            pos += 1
-        term_all_correlation[term_all] += 1
+def bf_correlate_to_dicts(term_term_correlation,
+                          term_all_correlation,
+                          basepath,
+                          treatment):
+    filename = basepath + "-" + treatment + ".csv"
+    with open(filename) as f:
+        reader = csv.reader(f)
+        for row in reader:
+            term_all = 0
+            pos = 0
+            for item in row:
+                if pos > 0 and pos % 2 == 0:
+                    correlation = int(item)
+                    term_all += correlation
+                    term_term_correlation[treatment][correlation] += 1
+                pos += 1
+            term_all_correlation[treatment][term_all] += 1
 
 def dict_to_csv(dd, filename):
     with open(filename, 'w') as f:
         writer = csv.writer(f)
-        for k,v in dd.items():
-            writer.writerow([k,v])
+        writer.writerow(["bucket","y","treatment"])
+        for treatment,subdict in dd.items():
+            for k, v in subdict.items():
+                writer.writerow([k,v,treatment])
 
+bf_correlate_to_dicts(term_term_correlation,
+                      term_all_correlation,
+                      "/tmp/correlate-150k",
+                      "rank3-rank0")
+bf_correlate_to_dicts(term_term_correlation,
+                      term_all_correlation,
+                      "/tmp/correlate-150k",
+                      "rank0")
 dict_to_csv(term_term_correlation, "/tmp/term-term.csv")
 dict_to_csv(term_all_correlation, "/tmp/term-all.csv")
