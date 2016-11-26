c5b498b005e6fc3964942f19f07a46372272c011
Dan Luu <danluu@gmail.com>
2016-11-25 16:00:03 -0800

    Add cachlines accessed to qwords plot.

--- src/Scripts/plot-qwords.r
+++ src/Scripts/plot-qwords.r
@@ -1,14 +1,30 @@
 library("ggplot2")
+library("reshape")
 setwd("~/dev/BitFunnel/src/Scripts")
 
-png(filename="qwords.png",width=1600,height=1200)
+
 queries <- read.csv(header=TRUE, file="/tmp/QueryPipelineStatistics.csv")
 pos = seq(1, length(queries$quadwords))
-df <- data.frame(pos, queries$quadwords)
+df_temp <- data.frame(pos, queries$quadwords, queries$cachelines)
+df <- melt(df_temp, id=c("pos"))
 
-ggplot(df, aes(x=pos,y=queries.quadwords)) +
+png(filename="qwords.png",width=1600,height=1200)
+ggplot(df, aes(x=pos,y=value,colour=factor(variable))) +
 theme_bw() +
-geom_point(alpha=1/100) +
+geom_point(alpha=1/150) +
+theme(axis.text = element_text(size=40),
+      axis.title = element_text(size=40),
+      legend.title=element_text(size=40),
+      legend.text=element_text(size=40))
+dev.off()
+
+df_temp$queries.quadwords <- df_temp$queries.quadwords / 4
+df <- melt(df_temp, id=c("pos"))
+
+png(filename="qwords-divided.png",width=1600,height=1200)
+ggplot(df, aes(x=pos,y=value,colour=factor(variable))) +
+theme_bw() +
+geom_point(alpha=1/150) +
 theme(axis.text = element_text(size=40),
       axis.title = element_text(size=40),
       legend.title=element_text(size=40),
@@ -16,12 +32,12 @@ theme(axis.text = element_text(size=40),
 dev.off()
 
 
-png(filename="qwords-solid.png",width=1600,height=1200)
-ggplot(df, aes(x=pos,y=queries.quadwords)) +
-theme_bw() +
-geom_point(alpha=1) +
-theme(axis.text = element_text(size=40),
-      axis.title = element_text(size=40),
-      legend.title=element_text(size=40),
-      legend.text=element_text(size=40))
-dev.off()
+# png(filename="qwords-solid.png",width=1600,height=1200)
+# ggplot(df, aes(x=pos,y=value)) +
+# theme_bw() +
+# geom_point(alpha=1) +
+# theme(axis.text = element_text(size=40),
+#       axis.title = element_text(size=40),
+#       legend.title=element_text(size=40),
+#       legend.text=element_text(size=40))
+# dev.off()
