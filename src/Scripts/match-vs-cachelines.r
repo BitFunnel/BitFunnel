7b6f2fc99b0e6f3f5a014143eaaa650bef3d5c0b
Dan Luu <danluu@gmail.com>
2016-12-01 15:25:05 -0800

    Add convenience scripts for cachelines vs. match time graph.

--- src/Scripts/match-vs-cachelines.r
+++ src/Scripts/match-vs-cachelines.r
@@ -2,13 +2,24 @@ library("ggplot2")
 library("reshape2")
 setwd("~/dev/BitFunnel/src/Scripts")
 
-interpreter <- read.csv(header=TRUE, file="/tmp/int/QueryPipelineStatistics.csv")
-compiler <- read.csv(header=TRUE, file="/tmp/comp/QueryPipelineStatistics.csv")
+args = commandArgs(trailingOnly=TRUE)
+if (length(args) != 3) {
+   stop("Required args: [interpreter QueryPipelineStats filename], [compiler QPS filename], [output filename]", call.=FALSE)
+}
+int_name = args[1]
+comp_name = args[2]
+out_name = args[3]
+
+print("Reading input.")
+interpreter <- read.csv(header=TRUE, file=int_name)
+compiler <- read.csv(header=TRUE, file=comp_name)
+
 df <- data.frame(interpreter$cachelines, compiler$match)
 names(df)[names(df) == 'interpreter.cachelines'] <- 'Cachelines'
 names(df)[names(df) == 'compiler.match'] <- 'MatchTime'
 
-png(filename="match-vs-cachelines.png",width=1600,height=1200)
+print("Plotting.")
+png(filename=out_name,width=1600,height=1200)
 ggplot(df, aes(x=Cachelines,y=MatchTime)) +
 theme_minimal() +
 geom_point(alpha=1/50) +
---
+++ src/Scripts/plot-cachelines-vs-time.sh
@@ -0,0 +1,1 @@
+Rscript match-vs-cachelines.r /tmp/int/QueryPipelineStatistics.csv /tmp/comp/QueryPipelineStatistics.csv cachelines-vs-time.png
