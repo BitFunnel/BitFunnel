fffbddf1e348c79e0f0c705936be7a64cd97dbc3
Dan Luu <danluu@gmail.com>
2016-12-01 15:12:51 -0800

    Combine interpreter and compiler results for cachelines vs. time graph
    This gives us a cacheline count (currently not implemented in compiled mode) as
    well as match times from the compiled results. It's possible that we'll see less
    noise in the chart as we ingest more documents. We could also try to reduce the
    noise by averaging over multiple runs.

--- src/Scripts/match-vs-cachelines.r
+++ src/Scripts/match-vs-cachelines.r
@@ -2,57 +2,17 @@ library("ggplot2")
 library("reshape2")
 setwd("~/dev/BitFunnel/src/Scripts")
 
-# See
-# https://www.r-bloggers.com/choosing-colour-palettes-part-ii-educated-choices/
-# for color information.
-
-df <- read.csv(header=TRUE, file="/tmp/QueryPipelineStatistics.csv")
-
-# queries <- read.csv(header=TRUE, file="/tmp/QueryPipelineStatistics.csv")
-# # Create column to graph vs. term position.
-# pos = seq(1, length(queries$quadwords))
-# df_temp <- data.frame(pos, queries$quadwords, queries$cachelines)
-
-# # Rename columns to have shorter names.
-# names(df_temp)[names(df_temp) == 'queries.quadwords'] <- 'quadwords'
-# names(df_temp)[names(df_temp) == 'queries.cachelines'] <- 'cachelines'
-
-# # Create dataframe in fully normalized form.
-# df <- melt(queries, measure.vars=c("match"), id.vars="cachelines")
-
-# Rename meaningless column name.
-# names(df)[names(df) == 'variable'] <- 'AccessType'
+interpreter <- read.csv(header=TRUE, file="/tmp/int/QueryPipelineStatistics.csv")
+compiler <- read.csv(header=TRUE, file="/tmp/comp/QueryPipelineStatistics.csv")
+df <- data.frame(interpreter$cachelines, compiler$match)
+names(df)[names(df) == 'interpreter.cachelines'] <- 'Cachelines'
+names(df)[names(df) == 'compiler.match'] <- 'MatchTime'
 
 png(filename="match-vs-cachelines.png",width=1600,height=1200)
-ggplot(df, aes(x=cachelines,y=match)) +
+ggplot(df, aes(x=Cachelines,y=MatchTime)) +
 theme_minimal() +
-geom_point(alpha=1/150) +
-ylim(0, 0.0001)
+geom_point(alpha=1/50) +
+theme(axis.text = element_text(size=40),
+      axis.title = element_text(size=40)) +
+ylim(0, 0.00004)
 dev.off()
-
-# queries$Quadwords <- queries$Quadwords / 8
-# df <- melt(queries, measure.vars=c("Quadwords","Cachelines","ExpectedCachelines"), id.vars="TermPos")
-# names(df)[names(df) == 'variable'] <- 'AccessType'
-
-# png(filename="qwords-divided.png",width=1600,height=1200)
-# ggplot(df, aes(x=TermPos,y=value,colour=AccessType)) +
-# scale_fill_brewer("Set1") + # doesn't work :-(
-# theme_minimal() +
-# geom_point(alpha=1/150) +
-# guides(colour = guide_legend(override.aes = list(alpha = 1))) +
-# theme(axis.text = element_text(size=40),
-#       axis.title = element_text(size=40),
-#       legend.title=element_text(size=40),
-#       legend.text=element_text(size=40))
-# dev.off()
-
-
-# # png(filename="qwords-solid.png",width=1600,height=1200)
-# # ggplot(df, aes(x=TermPos,y=value)) +
-# # theme_bw() +
-# # geom_point(alpha=1) +
-# # theme(axis.text = element_text(size=40),
-# #       axis.title = element_text(size=40),
-# #       legend.title=element_text(size=40),
-# #       legend.text=element_text(size=40))
-# # dev.off()
