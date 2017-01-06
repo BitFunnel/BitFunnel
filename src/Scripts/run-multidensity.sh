03bfdbc3bb098d3010e36533b30f4b310d8cd13c
Dan Luu <danluu@gmail.com>
2017-01-05 16:38:53 -0800

    Tweak multidensity scripting.

--- src/Scripts/run-multidensity.sh
+++ src/Scripts/run-multidensity.sh
@@ -5,25 +5,29 @@ set -x
 
 echo "Bash version ${BASH_VERSION}..."
 
-for dd in `seq 0.1 0.05 0.5`
+for dd in `seq 0.1 0.05 0.35`
 do
-    outline=$dd
+    # for treatment in PrivateSharedRank0 Optimal; do
+    outline=$treatment
+    outline+=","
+    outline+=$dd
     # Build TermTable
-    tools/BitFunnel/src/BitFunnel termtable ~/dev/wikipedia.100.200/config/ $dd PrivateSharedRank0
+    tools/BitFunnel/src/BitFunnel termtable ~/dev/wikipedia.100.150/config/ $dd Optimal
     outline+=","
-    outline+=`python3 ../src/Scripts/get-memory-usage.py  /home/danluu/dev/wikipedia.100.200/config/TermTableStatistics-0.txt`
+    outline+=`python3 ../src/Scripts/get-memory-usage.py  /home/danluu/dev/wikipedia.100.150/config/TermTableStatistics-0.txt`
 
-    # Run BitFunnel for output
-    tools/BitFunnel/src/BitFunnel repl ~/dev/wikipedia.100.200/config/ -script ~/dev/wikipedia.100.200/script.output
-    # Check output against verifier
-    python3 ../src/Scripts/verify.py ~/dev/wikipedia.100.200/d20.groundTruth.csv ~/dev/BitFunnel/build-ninja/verificationOutput.csv /tmp/verified.csv    
-    outline+=","
-    outline+=`python3 ..//src/Scripts/sum-csv-column.py /tmp/verified.csv 3`
+    # # Run BitFunnel for output
+    # tools/BitFunnel/src/BitFunnel repl ~/dev/wikipedia.100.200/config/ -script ~/dev/wikipedia.100.200/script.output
+    # # Check output against verifier
+    # python3 ../src/Scripts/verify.py ~/dev/wikipedia.100.200/d20.groundTruth.csv ~/dev/BitFunnel/build-ninja/verificationOutput.csv /tmp/verified.csv    
+    # outline+=","
+    # outline+=`python3 ..//src/Scripts/sum-csv-column.py /tmp/verified.csv 3`
 
     # Run BitFunnel for QPS
-    tools/BitFunnel/src/BitFunnel repl ~/dev/wikipedia.100.200/config/ -script ~/dev/wikipedia.100.200/script.query
+    tools/BitFunnel/src/BitFunnel repl ~/dev/wikipedia.100.150/config/ -script ~/dev/wikipedia.100.150/script.query.docfreq
     # 6th line, 2nd field should be QPS.
     outline+=","
     outline+=`awk 'FNR == 6 {print $2}' /tmp/QuerySummaryStatistics.txt`
     echo $outline >> /tmp/multidensity.csv
+    # done
 done    
--- src/Scripts/run-threads-vs-qps-multiple-treatments.sh
+++ src/Scripts/run-threads-vs-qps-multiple-treatments.sh
@@ -6,7 +6,7 @@ set -e
 set -x
 
 tools/BitFunnel/src/BitFunnel termtable /home/danluu/dev/wikipedia.100.150/config/ 0.1 PrivateSharedRank0 
-tools/BitFunnel/src/BitFunnel repl /home/danluu/dev/wikipedia.100.150/config/  -script /home/danluu/dev/wikipedia.100.150/script.query.generated.threads
+tools/BitFunnel/src/BitFunnel repl /home/danluu/dev/wikipedia.100.150/config/  -script /home/danluu/dev/wikipedia.100.150/script.query.docfreq.threads
 
 cp -r /tmp/threads /tmp/rank0/
 
@@ -16,6 +16,6 @@ cp -r /tmp/threads /tmp/rank0/
 # cp -r /home/danluu/dev/threads /home/danluu/dev/rank3rank0.100.150/
 
 tools/BitFunnel/src/BitFunnel termtable /home/danluu/dev/wikipedia.100.150/config/ 0.1 Optimal
-tools/BitFunnel/src/BitFunnel repl /home/danluu/dev/wikipedia.100.150/config/  -script /home/danluu/dev/wikipedia.100.150/script.query.generated.threads
+tools/BitFunnel/src/BitFunnel repl /home/danluu/dev/wikipedia.100.150/config/  -script /home/danluu/dev/wikipedia.100.150/script.query.docfreq.threads
 
 cp -r /tmp/threads /tmp/rankN/
