cf72c821636e94d955226b8f9c4ee5ff20578da1
Dan Luu <danluu@gmail.com>
2017-01-08 19:56:35 -0800

    Show more fine-grained per thread info.

--- src/Scripts/generate-queries-for-threads.py
+++ src/Scripts/generate-queries-for-threads.py
@@ -1,4 +1,4 @@
-for i in range(2, 17, 2):
+for i in range(1, 17):
     print("cd /tmp/threads/{}".format(i))
     print("threads {}".format(i))
     print("query log /home/danluu/dev/wikipedia.100.150/config/QueryLog.txt")
