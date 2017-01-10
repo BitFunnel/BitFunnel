7f05f102118547797ffedd08c1be11a8fa1aa460
Dan Luu <danluu@gmail.com>
2017-01-10 13:06:32 -0800

    Add SNR to multidensity output.

--- src/Scripts/run-multidensity.sh
+++ src/Scripts/run-multidensity.sh
@@ -6,7 +6,7 @@ set -x
 echo "Bash version ${BASH_VERSION}..."
 
 #outline="treatment,variant,density,bits,qps"
-outline="Treatment,Density,Bytes,QPS"
+outline="Treatment,Density,Bits,QPS,TruePos,FalsePos"
 echo $outline >> /tmp/multidensity.csv
 
 for dd in `seq 0.05 0.05 0.25`
@@ -20,12 +20,15 @@ do
     outline+=","
     outline+=`python3 ../src/Scripts/get-memory-usage.py  /home/danluu/dev/wikipedia.100.150/config/TermTableStatistics-0.txt`
 
-    # # Run BitFunnel for output
-    # tools/BitFunnel/src/BitFunnel repl ~/dev/wikipedia.100.200/config/ -script ~/dev/wikipedia.100.200/script.output
-    # # Check output against verifier
-    # python3 ../src/Scripts/verify.py ~/dev/wikipedia.100.200/d20.groundTruth.csv ~/dev/BitFunnel/build-ninja/verificationOutput.csv /tmp/verified.csv
-    # outline+=","
-    # outline+=`python3 ..//src/Scripts/sum-csv-column.py /tmp/verified.csv 3`
+    # Run BitFunnel for output
+    tools/BitFunnel/src/BitFunnel repl ~/dev/wikipedia.100.150/config/ -script ~/dev/wikipedia.100.150/script.output.docfreq
+    # Check output against verifier
+    python3 ../src/Scripts/verify.py ~/dev/wikipedia.100.150/d20.groundTruth.csv ~/dev/BitFunnel/build-ninja/verificationOutput.csv /tmp/verified.csv
+    true_pos=`python3 ..//src/Scripts/sum-csv-column.py /tmp/verified.csv 2`
+    false_pos=`python3 ..//src/Scripts/sum-csv-column.py /tmp/verified.csv 3`
+    outline+=","
+    outline+=`echo "scale=2; $true_pos/$false_pos" | bc`
+
 
     # Run BitFunnel for QPS
     tools/BitFunnel/src/BitFunnel repl ~/dev/wikipedia.100.150/config/ -script ~/dev/wikipedia.100.150/script.query.docfreq
