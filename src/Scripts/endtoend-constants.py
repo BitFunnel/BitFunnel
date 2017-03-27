ef6d88b1caa830a2b095d80dff7d32f624517149
Dan Luu <danluu@gmail.com>
2017-03-27 15:16:11 -0400

    Add script for generating simple chunk data.

---
+++ src/Scripts/endtoend-constants.py
@@ -0,0 +1,21 @@
+from num2words import num2words
+
+# Generate BitFunnel chunk data, with documents consisting of:
+# {'one'}, {'one', 'two'}, {'one', 'two', 'three'}, ...
+
+def print_numbers(num):
+    all_numbers = ""
+    for i in range(1, num+1):
+        english_number = num2words(i).replace(' ','-')
+        all_numbers += english_number + "\\0"
+    print(all_numbers)
+
+def print_doc(id, name):
+    print('\"{0:0>16x}\\0\"'.format(id))
+    print('\"01\\0{}\\0\" \"{}\\0\\0\"'.format(name, id))
+    print('\"00\\0\"')
+    print_numbers(id)
+    print('\"\\0\\0\"')
+
+for i in range (1, 64+1):
+    print_doc(i, 'Sequential')
