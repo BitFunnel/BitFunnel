807e5ff6d1c291f290db14aa06bb9c73e2a174c2
Mike Hopcroft <mhop@microsoft.com>
2018-04-20 17:09:51 -0700

    WIP: Refactoring ChunkWriter.
    This change is the first in a series that will allow FilterChunks
    to annotate documents with pseudo-terms that specify their shards.

---
+++ src/Chunks/src/ChunkWriters.cpp
@@ -0,0 +1,125 @@
+// The MIT License (MIT)
+
+// Copyright (c) 2016, Microsoft
+
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+#include "BitFunnel/Configuration/IShardDefinition.h"
+#include "BitFunnel/IFileManager.h"
+#include "BitFunnel/Index/IDocument.h"
+
+#include "ChunkWriters.h"
+
+
+namespace BitFunnel
+{
+    //*************************************************************************
+    //
+    // CopyingChunkWriter
+    //
+    //*************************************************************************
+    CopyingChunkWriter::CopyingChunkWriter(IFileManager& fileManager, size_t index)
+        : m_output(fileManager.Chunk(index).OpenForWrite())
+    {
+    }
+
+
+    CopyingChunkWriter::~CopyingChunkWriter()
+    {
+        *m_output << '\0';
+    }
+
+
+    void CopyingChunkWriter::Write(IDocument const &, char const * start, size_t length)
+    {
+        m_output->write(start, length);
+    }
+
+
+    //*************************************************************************
+    //
+    // CopyingChunkWriterFactory
+    //
+    //*************************************************************************
+    CopyingChunkWriterFactory::CopyingChunkWriterFactory(IFileManager& fileManager)
+        : m_fileManager(fileManager)
+    {
+    }
+
+
+    std::unique_ptr<IChunkWriter>
+        CopyingChunkWriterFactory::CreateChunkWriter(size_t index)
+    {
+        return std::make_unique<CopyingChunkWriter>(m_fileManager, index);
+    }
+
+
+    //*************************************************************************
+    //
+    // AnnotatingChunkWriter
+    //
+    //*************************************************************************
+    AnnotatingChunkWriter::AnnotatingChunkWriter(
+        IFileManager& fileManager,
+        size_t index,
+        IShardDefinition const & shardDefinition)
+        : CopyingChunkWriter(fileManager, index),
+        m_shardDefinition(shardDefinition)
+    {
+    }
+
+    void AnnotatingChunkWriter::Write(IDocument const & document,
+        char const * start,
+        size_t length)
+    {
+        // We need min/max count info about the shard the document's posting count belongs in
+        // (Note: We add one to the posting count to account for the added shard term)
+        auto shardId = m_shardDefinition.GetShard(document.GetPostingCount() + 1);
+        auto minCount = m_shardDefinition.GetMinPostingCount(shardId);
+        auto maxCount = m_shardDefinition.GetMaxPostingCount(shardId);
+
+        // Write out all of document except last document-ending byte
+        m_output->write(start, length - 1);
+
+        // Write out shard term into stream 00 (default for body)
+        *m_output << "00" << '\0'
+            << "SHARD_" << minCount << "_" << maxCount << '\0' << '\0' << '\0';
+    }
+
+
+    //*************************************************************************
+    //
+    // AnnotatingChunkWriterFactory
+    //
+    //*************************************************************************
+    AnnotatingChunkWriterFactory::AnnotatingChunkWriterFactory(
+        IFileManager& fileManager,
+        IShardDefinition const & shardDefinition)
+        : CopyingChunkWriterFactory(fileManager),
+          m_shardDefinition(shardDefinition)
+    {
+    }
+
+
+    std::unique_ptr<IChunkWriter>
+        AnnotatingChunkWriterFactory::CreateChunkWriter(size_t index)
+    {
+        return std::make_unique<AnnotatingChunkWriter>(m_fileManager, index, m_shardDefinition);
+    }
+}
---
+++ src/Chunks/src/ChunkWriters.h
@@ -0,0 +1,126 @@
+// The MIT License (MIT)
+
+// Copyright (c) 2016, Microsoft
+
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+#include <iosfwd>                   // std::ostream template parameter.
+#include <memory>                   // std::unique_ptr<T> parameter.
+#include <stddef.h>                 // size_t parameter.
+
+#include "BitFunnel/IInterface.h"   // Base class.
+#include "BitFunnel/NonCopyable.h"  // Base class.
+
+
+namespace BitFunnel
+{
+    class IDocument;
+    class IFileManager;
+    class IShardDefinition;
+
+
+    class IChunkWriter : public IInterface, NonCopyable
+    {
+    public:
+        virtual void Write(IDocument const & document,
+                           char const * start,
+                           size_t length) = 0;
+    };
+
+
+    class CopyingChunkWriter : public IChunkWriter
+    {
+    public:
+        CopyingChunkWriter(IFileManager& fileManager, size_t index);
+        virtual ~CopyingChunkWriter();
+
+
+        //
+        // IChunkWriter methods
+        //
+
+        virtual void Write(IDocument const & document,
+                           char const * start,
+                           size_t length) override;
+
+    protected:
+        std::unique_ptr<std::ostream> m_output;
+    };
+
+
+    class AnnotatingChunkWriter : public CopyingChunkWriter
+    {
+    public:
+        AnnotatingChunkWriter(IFileManager& fileManager,
+                              size_t index,
+                              IShardDefinition const & shardDefinition);
+
+        //
+        // IChunkWriter methods
+        //
+
+        virtual void Write(IDocument const & document,
+                           char const * start,
+                           size_t length) override;
+
+    private:
+        IShardDefinition const & m_shardDefinition;
+    };
+
+
+    class IChunkWriterFactory : public IInterface, NonCopyable
+    {
+    public:
+        virtual std::unique_ptr<IChunkWriter>
+            CreateChunkWriter(size_t index) = 0;
+    };
+
+
+    class CopyingChunkWriterFactory : public IChunkWriterFactory
+    {
+    public:
+        CopyingChunkWriterFactory(IFileManager& fileManager);
+
+        //
+        // IChunkWriterFactory methods.
+        //
+
+        virtual std::unique_ptr<IChunkWriter> CreateChunkWriter(size_t index) override;
+ 
+    protected:
+        IFileManager & m_fileManager;
+    };
+
+
+    class AnnotatingChunkWriterFactory : public CopyingChunkWriterFactory
+    {
+    public:
+        AnnotatingChunkWriterFactory(IFileManager& fileManager,
+                                     IShardDefinition const & shardDefinition);
+
+        //
+        // IChunkWriterFactory methods.
+        //
+
+        virtual std::unique_ptr<IChunkWriter> CreateChunkWriter(size_t index) override;
+
+    private:
+        IShardDefinition const & m_shardDefinition;
+    };
+}
